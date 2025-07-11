cf111999ccdf39bb22ea3e50cff25b451e4b037e
Drishti Baliyan <drishtibaliyan24@gmail.com>
2025-07-12 01:49:02 +0530

    Update and rename 5.js to LandscapeOrPortrait.js

--- LandscapeOrPortrait.js
+++ LandscapeOrPortrait.js
@@ -3,7 +3,7 @@ console.log(result);
 
 function isLandscape(width, height){
 
-    // return (width>height)? "Landscape" : "potrait";
+    // return (width>height)? "Landscape" : "portrait";
     return(width>height);
 
 }
